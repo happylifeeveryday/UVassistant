3d9b68f6be16cb8a53914fdb15f7d2e6248bf90d
Yihan Shang <97798597+happylifeeveryday@users.noreply.github.com>
2025-03-13 05:10:43 +0000

    Handle conflict by hand.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 </script>
 
 <template>
-  <!-- <nav>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
-  </nav> -->
-  <RouterView />
+  <div class="app-container">
+    <Header />
+    <router-view></router-view>
+    <Footer />
+  </div>
+
+
+
 </template>
 
 <style scoped></style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <footer class="bg-gray-600 text-white mt-auto">
+      <div class="max-w-7xl mx-auto px-4 py-4 flex justify-between items-center">
+        <a href="#" class="hover:text-gray-200">About Us and News</a>
+        <div class="flex items-center space-x-6">
+          <a href="#" class="hover:text-gray-200">Contact Us</a>
+          <div class="flex space-x-4">
+            <a href="#" class="hover:text-gray-200"><i class="fa-brands fa-facebook"></i></a>
+            <a href="#" class="hover:text-gray-200"><i class="fa-brands fa-twitter"></i></a>
+            <a href="#" class="hover:text-gray-200"><i class="fa-brands fa-pinterest"></i></a>
+            <a href="#" class="hover:text-gray-200"><i class="fa-brands fa-linkedin"></i></a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </template>
+  
